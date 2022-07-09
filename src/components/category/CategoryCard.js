a1a75c1f9bea8e2c6058bd1e63414ae400d00040
Steve Nagel <stevencnagel@gmail.com>
2022-07-09 14:36:02 -0500

    added category del

--- src/components/category/CategoryCard.js
+++ src/components/category/CategoryCard.js
@@ -1,9 +1,30 @@
 import react from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import "./CategoryCard.css"
 import "./../Rare.css"
+import { deleteCategory, getCategories } from "./CategoryManager";
+import { useEffect, useState } from "react";
 
 export const CategoryCard = ({ category }) => {
+    const history = useHistory()
+    const {categoryId} = useParams();
+    const [categories, setCategories] = useState([])
+
+    const loadCategories = () => {
+        getCategories().then(data => setCategories(data));
+    }
+    
+    useEffect(() => {
+        loadCategories();
+    }, []);
+
+    useEffect(()  => {
+        console.log(categories)
+    }, [categories])
+
+    const delCategory = (categoryId) => {
+        deleteCategory(categoryId)
+    }
 
     return (
         <>
@@ -12,8 +33,16 @@ export const CategoryCard = ({ category }) => {
                 <h3 className="categorycard__header__title">{category.label}</h3>
             </div>
             <div className="category_btn">
-                <button className="cardBtn" id="cardBbt__edit">✒️</button>
-                <button className="cardBtn" id="cardBbt__delete">🗑️</button>
+                <button 
+                    className="cardBtn" 
+                    id="cardBbt__edit">✒️</button>
+                <button 
+                    className="cardBtn" 
+                    id="cardBbt__delete"
+                    onClick={() => {
+                        delCategory(category.id)
+                        window.location.reload(false)
+                    }}>🗑️</button>
             </div>
         </section>
         </>
