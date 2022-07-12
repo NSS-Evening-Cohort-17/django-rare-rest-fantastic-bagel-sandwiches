c2c46cc9cf6579c382912501a891a4759f8fa40a
brianminges <btminges@gmail.com>
2022-07-11 20:09:53 -0500

    bm-5 tickets 4 and 5 function

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,38 +6,11 @@ import { PostList } from "./post/PostList"
 import { UserPostList } from "./post/UserPostList"
 import { CategoryList } from "./category/CategoryList"
 import { PostDetailComment } from "./post/PostDetailComment"
-import { CommentForm } from "./post/CommentForm"
+import { CommentForm } from "./comment/CommentForm"
 
 export const ApplicationViews = () => {
   return (
     <>
-<<<<<<< HEAD
-    <Route exact path="/posts">
-      <PostList />
-    </Route>
-    <Route exact path="/posts/new">
-      <PostForm />
-    </Route>
-    <Route exact path="/posts/my_posts">
-      <UserPostList />
-    </Route>
-    <Route exact path="/posts/:postId(\d+)">
-      <PostDetail />
-    </Route>
-    <Route exact path="/posts/:postId(\d+)/comments">
-      <PostDetailComment />
-    </Route>
-    <Route exact path="/my_posts/:postId(\d+)">
-      <PostDetail />
-    </Route>
-    <Route exact path="/my_posts/:postId(\d+)/comments">
-      <PostDetailComment />
-    </Route>
-    <Route exact path="/comments/new">
-      <CommentForm />
-    </Route>
-  </>
-=======
       <Route exact path="/posts">
         <PostList />
       </Route>
@@ -62,7 +35,9 @@ export const ApplicationViews = () => {
       <Route exact path="/my_posts/:postId(\d+)/comments">
         <PostDetailComment />
       </Route>
+      <Route exact path="/posts/comments/new/:postId(\d+)">
+        <CommentForm />
+      </Route>
     </>
->>>>>>> 823ad5465e7b13a1e773cc31cc933d8c6fc561a6
   )
 }
